6b9462d3e9932ae31251f34eee2142b4d4b2fee0
Md Imran Hossain <yaimran21@gmail.com>
2023-08-23 14:02:49 +0600

    upload art file

---
+++ art.py
@@ -0,0 +1,11 @@
+logo = '''                                                            
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88                                   '''
